25512d3c2230aa92435b4a9562b12f0cdeccefae
SamiRehmanCode <hsn.graphics007@gmail.com>
2023-06-30 21:40:41 +0500

    first commit

---
+++ random_gen_messg.js
@@ -0,0 +1,11 @@
+// Arrays containing different parts of the quotes
+const beginnings = [
+    "Believe in yourself",
+    "Dream big",
+    "Embrace change",
+    "Stay positive",
+    "Take risks",
+    "Never give up",
+  ];
+
+  
